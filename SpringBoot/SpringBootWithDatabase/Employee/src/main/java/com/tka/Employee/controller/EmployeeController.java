5de44cd480f3918199e984af55e8a751f7e63a0a
Kavina Kanse <kavinakanse@gmail.com>
2025-03-29 19:34:35 +0530

    Add files via upload

--- SpringBoot/SpringBootWithDatabase/Employee/src/main/java/com/tka/Employee/controller/EmployeeController.java
+++ SpringBoot/SpringBootWithDatabase/Employee/src/main/java/com/tka/Employee/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.tka.Employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tka.Employee.entity.Employee;
@@ -20,6 +24,19 @@ public class EmployeeController {
 	@PostMapping("/savedata")
 	public String insertData(@RequestBody Employee e) {
 		String msg = es.insertData(e);
+		return msg;
+	}
+	
+	@DeleteMapping("/deletedata")
+	public String deleteData(@RequestParam int empId) {
+		String msg = es.deleteData(empId);
+		return msg;
+		
+	}
+	
+	@PutMapping("/updatedata")
+	public String updateData(@RequestBody Employee e, @RequestParam int empId) {
+		String msg = es.updateData(e, empId);
 		return msg;
 	}
 }
