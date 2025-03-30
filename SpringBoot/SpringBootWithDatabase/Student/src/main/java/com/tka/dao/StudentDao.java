1b21af2cac312324104161288392776646bdc31d
Kavina Kanse <kavinakanse@gmail.com>
2025-03-30 20:48:42 +0530

    Add files via upload

--- SpringBoot/SpringBootWithDatabase/Student/src/main/java/com/tka/dao/StudentDao.java
+++ SpringBoot/SpringBootWithDatabase/Student/src/main/java/com/tka/dao/StudentDao.java
@@ -1,5 +1,6 @@
 package com.tka.dao;
 
+import com.tka.StudentApplication;
 import com.tka.entity.Student;
 
 import org.hibernate.Session;
@@ -11,8 +12,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class StudentDao {
 
+	private final StudentApplication studentApplication;
+
 	@Autowired
 	SessionFactory factory;
+
+	StudentDao(StudentApplication studentApplication) {
+		this.studentApplication = studentApplication;
+	}
 
 	public String insertData(Student s) {
 		Session ss = factory.openSession();
@@ -20,8 +27,38 @@ public class StudentDao {
 		ss.persist(s);
 		tr.commit();
 		ss.close();
-		
+
 		return "Data is inserted....";
-		
+
+	}
+
+	public String deleteData(int studId) {
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Student s = ss.get(Student.class, studId);
+		ss.remove(s);
+		tr.commit();
+		ss.close();
+
+		return "Data is deleted....";
+
+	}
+
+	public String updateData(Student s, int studId) {
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Student s1 = ss.get(Student.class, studId);
+		s1.setName(s.getName());
+		s1.setMarks(s.getMarks());
+		s1.setCity(s.getCity());
+
+		ss.merge(s1);
+		tr.commit();
+		ss.close();
+
+		return "Data is updated....";
+
 	}
 }
