74f4f18c9d33fc74e279dfaec911b4528b953b46
Kavina Kanse <kavinakanse@gmail.com>
2025-04-01 11:41:27 +0530

    Add files via upload

--- SpringBoot/SpringBootWithDatabase/Employee/src/main/java/com/tka/Employee/controller/EmployeeController.java
+++ SpringBoot/SpringBootWithDatabase/Employee/src/main/java/com/tka/Employee/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.tka.Employee.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -39,4 +42,17 @@ public class EmployeeController {
 		String msg = es.updateData(e, empId);
 		return msg;
 	}
+	
+	@GetMapping("/getsingledata")
+	public Employee getSingleRecord(@RequestParam int empId) {
+		Employee e = es.getSingleRecord(empId);
+		return e;
+	}
+	
+	@GetMapping("/getallrecord")
+	public List<Object> getAllRecord(Employee e) {
+		List<Object> list = es.getAllRecord(e);
+		return list;
+	}
+	
 }
